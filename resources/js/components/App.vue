bf5bee0eebb7a384605737e473cb662053036b25
Adrian Rahmandanu <mochamad.rahmandanu@mekari.com>
2025-11-30 23:22:27 +0700

    setup vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <example-component></example-component>
+    </div>
+</template>
+
+<script>
+import ExampleComponent from './ExampleComponent.vue';
+
+export default {
+  components: {
+    ExampleComponent,
+  },
+  // Anda bisa menambahkan data, methods, dll. di sini
+};
+</script>
